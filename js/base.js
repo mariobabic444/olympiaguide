1286ff4c34ba991bd47ef0a7298153ade25005df
Mario <mariobabic444@gmail.com>
2020-09-16 18:00:47 +0200

    first

---
+++ js/base.js
@@ -0,0 +1,12 @@
+var OlympiaGuide = {
+    init: function () {
+    },
+    template: function (template, data) {
+        return template.replace(/{([\w.]*)}/g, function (str, key) {
+            var keys = key.split("."), v = data[keys.shift()];
+            for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+            return (typeof v !== "undefined" && v !== null) ? v : "";
+        });
+    },
+};
+OlympiaGuide.init();
